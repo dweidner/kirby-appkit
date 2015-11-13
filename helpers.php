1212ea550b0678d67dd39610e4c651de52a99165
Daniel Weidner <d.weidner@hks-agentur.de>
2015-11-13 13:24:58 +0100

    Add a helper function that can be used to load a tempalte partial.

--- helpers.php
+++ helpers.php
@@ -8,3 +8,23 @@
 function app() {
   return Appkit\App::instance();
 }
+
+/**
+ * Embeds a partial from the application folder.
+ *
+ * @param   string   $file
+ * @param   mixed    $data
+ * @param   boolean  $return
+ *
+ * @return  string
+ */
+function partial( $file, $data = array(), $return = false ) {
+
+  if( is_object( $data ) ) {
+    $data = array( 'item' => $data );
+  }
+
+  $file = app()->finder()->partials() . DS . $file . '.php';
+  return tpl::load( $file, $data, $return );
+
+}
